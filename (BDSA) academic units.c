29a6fdcfef3232698884c16d330718e94dba6e8d
Amos kipkurui <kipkuruiamos@students.zetech.ac.ke>
2026-01-24 10:24:25 +0300

    Add files via upload

---
+++ (BDSA) academic units.c
@@ -0,0 +1,20 @@
+//program to display academic units offered this semester
+/*
+Description:program to display academic units offered this semester
+Name:AMOS KIPKURUI
+Reg no:BDSA-03-0013/2026
+*/
+#include<stdio.h>
+//main function
+int main(){
+	printf("\tacademic units offered this semester:\n");
+	printf("1.communication skills\n");
+	printf("2.digital literacy\n");
+	printf("3.health and wellness management\n");
+	printf("4.data ethics,privacy and governance\n");
+	printf("5.basic mathematics\n");
+	printf("6.structured programming and algorithms\n");
+	
+	
+	
+}
